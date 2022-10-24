526764ec2e32db9a0bdff35c4b321e8e04638131
Arxaid <VallravenWasTaken@gmail.com>
2022-10-24 10:07:08 +0300

    Moved datasheets into new folder and untracked

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ import ArxPy as arx
 
 if __name__ == '__main__':
 
-    datasheet1 = arx.DataLoading_txt('VAR1.txt')
-    datasheet2 = arx.DataLoading_txt('VAR3.txt')
-    datasheet3 = arx.DataLoading_txt('VAR6.txt')
-    datasheet4 = arx.DataLoading_txt('VAR9.txt')
+    datasheet1 = arx.DataLoading_txt('datasheets/VAR1.txt')
+    datasheet2 = arx.DataLoading_txt('datasheets/VAR3.txt')
+    datasheet3 = arx.DataLoading_txt('datasheets/VAR6.txt')
+    datasheet4 = arx.DataLoading_txt('datasheets/VAR9.txt')
     alpha = 0.05
 
     print('\nStudent t-test\nVAR1 and VAR3')
